29d48b6dc46e82d2833126b375401d2b3376a1a1
Luís Fialho <luisfelipedesousafialho@gmail.com>
2026-06-17 21:42:27 -0300

    Update Anfitriao.java

--- backend/model/Anfitriao.java
+++ backend/model/Anfitriao.java
@@ -1,7 +1,7 @@
-package model;
-
 public class Anfitriao extends Usuario {
+    
     public Anfitriao(int id, String nome, String email, String senha) {
+        // A palavra 'super' chama o construtor da classe mãe (Usuario)
         super(id, nome, email, senha, TipoConta.ANFITRIAO);
     }
 }
