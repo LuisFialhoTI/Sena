6052203e305b83bfb6e02a80931e850977f0920a
Luís Fialho <luisfelipedesousafialho@gmail.com>
2026-06-17 21:33:58 -0300

    Update SenaException.java

--- backend/exception/SenaException.java
+++ backend/exception/SenaException.java
@@ -1,10 +1,11 @@
-package exception;
-
 public class SenaException extends Exception {
+
+    // 1º Construtor: Recebe apenas a mensagem de erro (1 parâmetro)
     public SenaException(String message) {
         super(message);
     }
 
+    // 2º Construtor (Sobrecarga): Recebe a mensagem e a causa original do erro (2 parâmetros)
     public SenaException(String message, Throwable cause) {
         super(message, cause);
     }
